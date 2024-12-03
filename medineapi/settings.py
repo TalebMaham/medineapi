8ed794af2f8b824e5ba669270a1b673dffc14bf9
TalebMaham <sidnas9@gmail.com>
2024-12-03 21:24:57 +0100

    medine api

--- medineapi/settings.py
+++ medineapi/settings.py
@@ -61,13 +61,16 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'medineapi.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
-    # 'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+
 }
 
 
